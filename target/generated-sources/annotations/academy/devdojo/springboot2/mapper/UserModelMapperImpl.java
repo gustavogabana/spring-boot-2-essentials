0a8c451a8e3cda75468393461837927c7ac2471e
Luiz Gustavo <Gustavo_97@protonmail.com>
2023-08-27 17:22:33 -0300

    Ajuste para o banco correto.

--- target/generated-sources/annotations/academy/devdojo/springboot2/mapper/UserModelMapperImpl.java
+++ target/generated-sources/annotations/academy/devdojo/springboot2/mapper/UserModelMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-19T16:03:11-0300",
+    date = "2023-08-27T17:18:17-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
 )
 @Component
